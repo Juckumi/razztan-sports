100ca9e12c09deaa51afb5694fe1848def90e70b
Jair Quintana <22.daw.jairquintana@fundacionmontessori.com>
2024-04-19 19:37:51 +0200

    se ha establecido el router provider y algunas configuraciones de eslint

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,11 +1,8 @@
 
-import AppLayout from "../app/AppLayout";
 
 function LandingPage() {
     return (
-        <AppLayout>
             <h3>Esta es la landing Pages</h3>
-        </AppLayout>
     )
 }
 
--- client/src/pages/UserDashboard.jsx
+++ client/src/pages/UserDashboard.jsx
@@ -1,10 +1,7 @@
-import AppLayout from "../app/AppLayout";
 
 function UserDashboard() {
     return (
-        <AppLayout>
         <h3>Esta es la UserDashboard</h3>
-    </AppLayout>
     )
 }
 
