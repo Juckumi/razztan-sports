406372f3f135cae7b1ce29d91d42614d87a11391
Jair Quintana <22.daw.jairquintana@fundacionmontessori.com>
2024-05-01 13:06:05 +0200

    se han creado las vistas del formulario de login

--- client/src/features/identify/RegisterForm.jsx
+++ client/src/features/identify/RegisterForm.jsx
@@ -4,6 +4,9 @@ import InputForm from "../../ui/InputForm"
 import { FiUser,FiLock,FiMail   } from "react-icons/fi";
 import Logo from "../../ui/Logo";
 import Button from "../../ui/Button";
+import { createUser } from "../../api/userApi";
+import { useState } from "react";
+import { useNavigate } from "react-router";
 
 const rules = {
   email:  new RegExp("^[^\\s@]+@[^\\s@]+\\.[^\\s@]+$"),
@@ -12,31 +15,60 @@ const rules = {
 }
 
 
+
 function RegisterForm() {
-  const handleSubmit = (e) => {
+
+  const [formData, setFormData] = useState({
+    name:'',
+    surname:'',
+    username:'',
+    email:'',
+    pwd:'',
+    confirmPwd:'',
+  });
+  const navigate = useNavigate();
+  const [errors, setErrors] = useState(null);
+  const handleSubmit = async (e) => {
+    console.log(e)
     e.preventDefault(); 
-        console.log('holaaa')
+
+      const [res,data ]= await createUser(formData);
+
+      console.log(res,data,'resssssssss')
+
+        // if (res.ok && res.status === 201) {
+        //   navigate('/user/dashboard'); 
+        //  } else {
+        //   setErrors(data.errors);
+        //  }
+       
+        //  console.log(errors)
   }
+  
+
     return (
       <>       
       <Logo isWhite={true} />      
        <Form onSubmit={handleSubmit}>
                 <h1 style={{color:'var(--color-brand-green-500)',fontSize:'2rem',margin:'1rem'}}>Registrarse</h1>
                
-                <FormRow >
-                    <InputForm label='Usuario' name='user' type='text' icon={<FiUser />}/>
-                    <InputForm label='Email' name='email' type='text' icon={<FiMail />} rule={rules.email}/>
+                  <FormRow >
+                    <InputForm setFormData={setFormData} formData={formData} label='Usuario' name='username' type='text' icon={<FiUser />}/>
+                    <InputForm setFormData={setFormData}  formData={formData} label='Email' name='email' type='text' icon={<FiMail />} rule={rules.email}/>
                   </FormRow>
-              <FormRow >
-                    <InputForm label='Nombre' name='name' type='text' icon=''/>
-                    <InputForm label='Apellidos' name='surname' type='text' icon='' />
-
+                  <FormRow >
+                      <InputForm  setFormData={setFormData}  formData={formData} label='Nombre' name='name' type='text' icon=''/>
+                      <InputForm setFormData={setFormData}  formData={formData} label='Apellidos' name='surname' type='text' icon='' />
                   </FormRow>
-              <FormRow >
-                    <InputForm label='Contraseña' name='passwd' type='password' icon={<FiLock />} rule={rules.contraseña}/>
-                    <InputForm label='Confirmar Contraseña' name='confirmPasswd' type='password' icon='' />
-
+                  <FormRow >
+                      <InputForm setFormData={setFormData}  formData={formData} label='Contraseña' name='pwd' type='text' icon={<FiLock />} rule={rules.contraseña}/>
+                      <InputForm setFormData={setFormData}  formData={formData} label='Confirmar Contraseña' name='confirmPwd' type='text' icon='' />
                   </FormRow>
+                  { errors !== null && Object.entries(errors).map(([campo, array]) => (
+                          <p key={campo}>
+                            {campo}: {array.join(', ')}
+                          </p>
+      ))}
                    <Button>Registrarse</Button>
               </Form>
               </>
--- client/src/ui/Button.jsx
+++ client/src/ui/Button.jsx
@@ -1,12 +1,13 @@
 import styled from "styled-components"
 
-const StyledButton = styled.button`
+const Button = styled.button`
  border-radius: var(--b-radius-lg);
  background-color: var(--color-brand-beige-500);
  color: var(--color-brand-bone-200);
  padding: 1rem 2rem;
  font-size: 1.3rem;
  transition: transform 1s;
+ border: none;
 
  &&:hover{
     transform: scale(1.1);
@@ -14,13 +15,5 @@ const StyledButton = styled.button`
 
 `
 
-
-function Button({children}) {
-    return (
-        <StyledButton>
-            {children}
-        </StyledButton>
-    )
-}
 
 export default Button
